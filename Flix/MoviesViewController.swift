2d81634b3f1e182b208a746e4affec6268b2e73f
PennyThePenniless <84546597+PennyThePenniless@users.noreply.github.com>
2021-09-20 23:42:14 -0400

    Flix part 2
    Now users can select movies in the now playing section and find out more info about the movie. Users can also now view a list of superhero movies.

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -10,7 +10,6 @@ import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-
     @IBOutlet weak var tableView: UITableView!
     var movies = [[String: Any]]()
     
@@ -62,14 +61,27 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //Find the selected movie
+        
+        //Pass the selected movie to the details view controller
+        
+        let cell =  sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
---
+++ Flix/TestViewController.swift
@@ -0,0 +1,47 @@
+//
+//  TestViewController.swift
+//  Flix
+//
+//  Created by ELYS on 9/20/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class TestViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //print(movie["title"] as Any)
+        
+        /*titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        synopsisLabel.text = movie["title"] as? String
+        synopsisLabel.sizeToFit()
+        let baseUrl = "https://image.tmdb.org/t/p/w780"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        posterView.af.setImage(withURL: posterUrl!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: baseUrl + backdropPath)
+        
+        backdropView.af.setImage(withURL: backdropUrl!)*/
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
